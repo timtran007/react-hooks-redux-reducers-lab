8104fc44456e14004903400780e533024f1f2ede
Tim <timtran007@gmail.com>
2023-02-12 19:17:07 -0600

    commit tree 9a59f41ab3f98ce8d747c08f948c8361e1ae4cc9

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,18 @@
-export function manageFriends(state, action) {
-  // your code here
+import { Binding } from "@babel/traverse"
+
+const initialState = {friend: []}
+export function manageFriends(state = initialState, action) {
+  console.log(state)
+  switch(action.type) {
+    case 'friends/add':
+      return {
+        name: state.name,
+        hometown: state.hometown,
+        id: state.id, 
+      }
+    case 'friends/remove':
+      return
+    default:
+      return state
+  }
 }
